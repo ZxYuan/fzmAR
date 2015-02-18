7f0f136e120aa44369e9db385849492e6e52693a
ZxYuan <lvp0526@gmail.com>
2015-02-19 02:58:55 +0900

    Hajimeni

---
+++ fzmAR/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.Util;
+
+namespace fzmAR
+{
+    public partial class Form1 : Form
+    {
+        private Capture _capture = null;
+        private bool _captureInProgress;
+
+        private HandTracker handtracker = new HandTracker();
+
+        Image<Bgr, Byte> sword = new Image<Bgr, byte>(@"data/lightsword.png");
+
+        public Form1()
+        {
+            InitializeComponent();
+            try
+            {
+                _capture = new Capture();
+                _capture.ImageGrabbed += ProcessFrame;
+            }
+            catch (NullReferenceException excpt)
+            {
+                MessageBox.Show(excpt.Message);
+            }
+        }
+
+        private void ProcessFrame(object sender, EventArgs arg)
+        {
+            Image<Bgr, Byte> frame = _capture.RetrieveBgrFrame();
+            Image<Bgr, Byte> rawFrame = frame.Copy();
+            Image<Gray, Byte> grayFrame = frame.Convert<Gray, Byte>();
+            Rectangle rect1 = handtracker.DetectFist(grayFrame.Bitmap);
+            Rectangle rect2 = handtracker.DetectPalm(grayFrame.Bitmap);
+            if (true || rect1.Width > 0)
+            {
+                Console.WriteLine("fist");
+                Console.WriteLine(rect1.Width+", "+rect1.Height);
+                Image<Bgr, Byte> curSword = sword.Resize(108, 94, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+                Rectangle roi = new Rectangle(rect1.X, rect1.Y, curSword.Cols, curSword.Rows);
+                frame.ROI = roi;
+                Image<Bgr, Byte> roiImage = frame.Copy();
+                curSword = curSword.Add(roiImage);
+                curSword.CopyTo(frame);
+                frame.ROI = Rectangle.Empty;
+            }
+            if (rect2.Width > 0)
+            {
+                Console.WriteLine("palm");
+            }
+            imageBox2.Image = frame;
+            imageBox1.Image = rawFrame;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (_capture != null)
+            {
+                if (_captureInProgress)
+                {  //stop the capture
+                    button1.Text = "Start";
+                    _capture.Pause();
+                }
+                else
+                {
+                    //start the capture
+                    button1.Text = "Stop";
+                    _capture.Start();
+                }
+
+                _captureInProgress = !_captureInProgress;
+            }
+        }
+
+        private void ReleaseData()
+        {
+            if (_capture != null)
+                _capture.Dispose();
+        }
+    }
+}
---
+++ fzmAR/HandTracker.cs
@@ -0,0 +1,100 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace fzmAR
+{
+    public class HandTracker : IDisposable
+    {
+        CascadeClassifier palmHaar = new CascadeClassifier(@"data/palm.dat");
+        CascadeClassifier fistHaar = new CascadeClassifier(@"data/fist.dat");
+
+        public void Dispose()
+        {
+            if (palmHaar != null)
+            {
+                palmHaar.Dispose();
+                palmHaar = null;
+            }
+            if (fistHaar != null)
+            {
+                fistHaar.Dispose();
+                fistHaar = null;
+            }
+        }
+
+        public Rectangle DetectPalm(string imagePath)
+        {
+            if (!File.Exists(imagePath))
+            {
+                return new Rectangle(0, 0, 0, 0);
+            }
+            Image<Bgr, Byte> img = new Image<Bgr, byte>(imagePath);
+            Image<Gray, Byte> grayImage = img.Copy().Convert<Gray, byte>();
+            Rectangle[] palmDetected = palmHaar.DetectMultiScale(grayImage, 1.4, 10, new Size(20, 20), Size.Empty);
+            //faces.AddRange(facesDetected);
+            if (palmDetected == null || palmDetected.Length == 0)
+            {
+                return new Rectangle(0, 0, 0, 0);
+            }
+
+            return palmDetected[0];
+        }
+        public Rectangle DetectPalm(Bitmap bitmap)
+        {
+            if (bitmap == null)
+            {
+                return new Rectangle(0, 0, 0, 0);
+            }
+            Image<Bgr, Byte> img = new Image<Bgr, byte>(bitmap);
+            Image<Gray, Byte> grayImage = img.Copy().Convert<Gray, byte>();
+            Rectangle[] palmDetected = palmHaar.DetectMultiScale(grayImage, 1.4, 10, new Size(20, 20), Size.Empty);
+            //faces.AddRange(facesDetected);
+            if (palmDetected == null || palmDetected.Length == 0)
+            {
+                return new Rectangle(0, 0, 0, 0);
+            }
+
+            return palmDetected[0];
+        }
+        public Rectangle DetectFist(string imagePath)
+        {
+            if (!File.Exists(imagePath))
+            {
+                return new Rectangle(0, 0, 0, 0);
+            }
+            Image<Bgr, Byte> img = new Image<Bgr, byte>(imagePath);
+            Image<Gray, Byte> grayImage = img.Copy().Convert<Gray, byte>();
+            Rectangle[] fistDetected = fistHaar.DetectMultiScale(grayImage, 1.4, 10, new Size(20, 20), Size.Empty);
+            if (fistDetected == null || fistDetected.Length == 0)
+            {
+                return new Rectangle(0, 0, 0, 0);
+            }
+
+            return fistDetected[0];
+        }
+
+        public Rectangle DetectFist(Bitmap bitmap)
+        {
+            if (bitmap == null)
+            {
+                return new Rectangle(0, 0, 0, 0);
+            }
+            Image<Bgr, Byte> img = new Image<Bgr, byte>(bitmap);
+            Image<Gray, Byte> grayImage = img.Copy().Convert<Gray, byte>();
+            Rectangle[] fistDetected = fistHaar.DetectMultiScale(grayImage, 1.4, 10, new Size(20, 20), Size.Empty);
+            if (fistDetected == null || fistDetected.Length == 0)
+            {
+                return new Rectangle(0, 0, 0, 0);
+            }
+
+            return fistDetected[0];
+        }
+    }
+}
